33b9142a15a5939d0932b80b5ef9e871ae5c065b
okejsew <okejsew@gmail.com>
2026-04-15 14:35:32 +1000

    renamed all variables to unified style

--- RythmMemory/main.cpp
+++ RythmMemory/main.cpp
@@ -1,6 +1,5 @@
 #include <SFML/Graphics.hpp>
-#include <optional>// ??? Ответ: обяснено в код блоке выше, 
-//используется в while (const std::optional event = window.pollEvent())
+#include <optional>
 #include <vector>
 #include <algorithm>
 #include <random>
@@ -8,17 +7,17 @@
 #include <map>
 
 
-const unsigned int count_all_cards = 8;
+const unsigned int allCardsCount = 8;
 
-enum class CardStatus { open, close, discarded };
-enum class CardType { sunflower, gentian, ghost_pipe, waterlily };
+enum class CardStatus { opened, closed, discarded };
+enum class CardType { sunflower, gentian, ghostpipe, waterlily };
 std::vector<CardType> types = {
     CardType::sunflower,
     CardType::sunflower,
     CardType::gentian,
     CardType::gentian,
-    CardType::ghost_pipe,
-    CardType::ghost_pipe,
+    CardType::ghostpipe,
+    CardType::ghostpipe,
     CardType::waterlily,
     CardType::waterlily
 };
@@ -26,8 +25,8 @@ std::vector<CardType> types = {
 
 std::ostream& operator<<(std::ostream& os, CardStatus status) { //две функции для вывода в cout новых типов
     switch (status) {
-    case CardStatus::open: return os << "open";
-    case CardStatus::close: return os << "close";
+    case CardStatus::opened: return os << "open";
+    case CardStatus::closed: return os << "close";
     case CardStatus::discarded: return os << "discarded";
     }
     return os;
@@ -36,7 +35,7 @@ std::string toString(CardType type) {
     switch (type) {
     case CardType::sunflower: return "sunflower";
     case CardType::gentian: return "gentian";
-    case CardType::ghost_pipe: return "ghost_pipe";
+    case CardType::ghostpipe: return "ghostpipe";
     case CardType::waterlily: return "waterlily";
     }
     return "unknown";
@@ -49,6 +48,7 @@ public:
     CardType type; // растения
     CardStatus status; // открыто, закрыто, сброшено
     sf::RectangleShape shape; // визуальное представление карты, которое будет отриовано в окне
+    float timer = 0.f; // время после изменения состояния
 
     Card() //конструктор по умолчанию - если не было передано параметров, нужен
         // чтобы создать массив карт без заполнения данных
@@ -57,15 +57,15 @@ public:
         shape.setFillColor(sf::Color::Green); //задать цвет заливки карты
     }
 
-    Card(CardType p_type, CardStatus p_status) // конструктор с параметрами
+    Card(CardType cardType, CardStatus cardStatus) // конструктор с параметрами
     {
-        type = p_type;
-        status = p_status;
+        type = cardType;
+        status = cardStatus;
         shape.setSize({ 250.f, 350.f });
         shape.setFillColor(sf::Color::Green);
     }
 
-    float timer = 0.f; // время после изменения состояния
+    
 
 
     void draw(sf::RenderWindow& window) {//функция отрисовки карты добавлена в класс для удобства
@@ -75,7 +75,7 @@ public:
 
 };
 
-void full_table(Card Cards[]) //фунция разложения стола
+void dealCards(Card Cards[]) //фунция разложения стола
 {
     //Копируем types 
     std::vector<CardType> shuffledTypes = types;
@@ -86,16 +86,16 @@ void full_table(Card Cards[]) //фунция разложения стола
     std::shuffle(shuffledTypes.begin(), shuffledTypes.end(), g);
 
     // Заполняем карты
-    for (unsigned int i = 0; i < count_all_cards; i++)
+    for (unsigned int i = 0; i < allCardsCount; i++)
     {
-        Cards[i].status = CardStatus::close;
+        Cards[i].status = CardStatus::closed;
         Cards[i].type = shuffledTypes[i];
     }
 }
 
-bool is_pair(Card open_card1, Card open_card2)// проверка карт на парность
+bool isPair(Card card1, Card card2)// проверка карт на парность
 {
-    if (open_card1.type == open_card2.type)
+    if (card1.type == card2.type)
         return 1;
     return 0;
 }
@@ -115,12 +115,12 @@ int main()
 
     sf::Texture sunflowerTexture; // создание переменных, где будут хранится текстуры
     sf::Texture gentianTexture;
-    sf::Texture ghost_pipeTexture;
+    sf::Texture ghostpipeTexture;
     sf::Texture waterlilyTexture;
     std::map<CardType, sf::Texture> textures; // создание и заполнение словоря (тип карты -> текстура)
     textures[CardType::sunflower].loadFromFile("../Assets/sunflower.png");
     textures[CardType::gentian].loadFromFile("../Assets/gentian.png");
-    textures[CardType::ghost_pipe].loadFromFile("../Assets/ghost_pipe.png");
+    textures[CardType::ghostpipe].loadFromFile("../Assets/ghostpipe.png");
     textures[CardType::waterlily].loadFromFile("../Assets/waterlily.png");
 
 
@@ -128,21 +128,21 @@ int main()
     sf::Font font;
     font.openFromFile("../Fonts/BRLNSR.TTF");//подгрузка шрифтов
 
-    sf::Text points_txt(font); // создание объекта текст для вывода очков
-    points_txt.setCharacterSize(100);
-    points_txt.setFillColor(sf::Color::White);
+    sf::Text txtPoints(font); // создание объекта текст для вывода очков
+    txtPoints.setCharacterSize(100);
+    txtPoints.setFillColor(sf::Color::White);
 
-    sf::Text timer_txt(font); // создание объекта текст для вывода таймера
-    timer_txt.setCharacterSize(100);
-    timer_txt.setFillColor(sf::Color::White);
+    sf::Text txtTimer(font); // создание объекта текст для вывода таймера
+    txtTimer.setCharacterSize(100);
+    txtTimer.setFillColor(sf::Color::White);
 
 
 
-    Card Cards[count_all_cards]; // создание массива с картами, все карты созданы без параметров
+    Card cards[allCardsCount]; // создание массива с картами, все карты созданы без параметров
 
-    for (unsigned int i = 0; i < count_all_cards; i++)
+    for (unsigned int i = 0; i < allCardsCount; i++)
     {
-        Cards[i].status = CardStatus::discarded; // присвоение всем картам статуса сброшено
+        cards[i].status = CardStatus::discarded; // присвоение всем картам статуса сброшено
         // это происходит до создания окна и зацикливания программы.
         // это необходимо, т.к. условие раскладывания нового стола - это все карты сброшены.
         // при первом проходе программа пройдёт почти до конца ничего не выполнив, так с сброшенными картами
@@ -151,7 +151,7 @@ int main()
 
 
 
-    Cards[0].shape.setPosition({ 100.f, 100.f }); // лишняя строка, забыл удалить
+    cards[0].shape.setPosition({ 100.f, 100.f }); // лишняя строка, забыл удалить
 
 
 
@@ -163,8 +163,8 @@ int main()
 
 
 
-    unsigned int count_open_cards = 0; // подсчёт кол-ва открытых карт
-    unsigned int open_card1, open_card2; // индексы карт в массиве Cards которые сейчас открыты
+    unsigned int openedCardsCount = 0; // подсчёт кол-ва открытых карт
+    unsigned int openedCard1, openedCard2; // индексы карт в массиве cards которые сейчас открыты
     unsigned int points = 0; // очки
 
     sf::Clock clock; // создание часов
@@ -184,10 +184,10 @@ int main()
 
             if (waitTimer >= 1.f) // если подождали 1 секунду
             {
-                Cards[open_card1].status = CardStatus::close;//закрываем карты
-                Cards[open_card2].status = CardStatus::close;
+                cards[openedCard1].status = CardStatus::closed;//закрываем карты
+                cards[openedCard2].status = CardStatus::closed;
 
-                count_open_cards = 0; // открытых карт нет 
+                openedCardsCount = 0; // открытых карт нет 
                 waiting = false; // больше не ждём
             }
         }
@@ -208,28 +208,28 @@ int main()
                 {
                     sf::Vector2f mousePos = window.mapPixelToCoords(
                         { mouseEvent->position.x, mouseEvent->position.y });//  координаты нажатия
-                    for (int i = 0; i < count_all_cards; i++)
+                    for (int i = 0; i < allCardsCount; i++)
                     {
-                        if (Cards[i].shape.getGlobalBounds().contains(mousePos) && (Cards[i].status == CardStatus::close)) //координаты нажатия совпадают с областью карты
+                        if (cards[i].shape.getGlobalBounds().contains(mousePos) && (cards[i].status == CardStatus::closed)) //координаты нажатия совпадают с областью карты
                         {
-                            Cards[i].status = CardStatus::open;// изменить статус на открыта
-                            count_open_cards += 1; // увеличить кол-во открытых сейчас карт
-                            if (count_open_cards == 1) // если открыта 1 карта
+                            cards[i].status = CardStatus::opened;// изменить статус на открыта
+                            openedCardsCount += 1; // увеличить кол-во открытых сейчас карт
+                            if (openedCardsCount == 1) // если открыта 1 карта
                             {
-                                open_card1 = i; // запомнить её индекс
+                                openedCard1 = i; // запомнить её индекс
                             }
-                            else if (count_open_cards == 2) // если открыто 2 карты
+                            else if (openedCardsCount == 2) // если открыто 2 карты
                             {
-                                open_card2 = i; // запомнить её индекс
-                                if (is_pair(Cards[open_card1], Cards[open_card2])) //проверить на парность
+                                openedCard2 = i; // запомнить её индекс
+                                if (isPair(cards[openedCard1], cards[openedCard2])) //проверить на парность
                                 {
                                     //dt = clock.restart().asSeconds();
 
                                     points += 100;//начислить очки за пару
-                                    Cards[open_card1].status = CardStatus::discarded;//карты сделать сброшенными
-                                    Cards[open_card2].status = CardStatus::discarded;
+                                    cards[openedCard1].status = CardStatus::discarded;//карты сделать сброшенными
+                                    cards[openedCard2].status = CardStatus::discarded;
 
-                                    count_open_cards = 0;//обнулить кол-во открытых карт
+                                    openedCardsCount = 0;//обнулить кол-во открытых карт
                                 }
                                 else
                                 {
@@ -246,51 +246,51 @@ int main()
             }
         }
 
-        unsigned int count_discarded_cards = 0;//счётчик сброшенных карт
-        for (int i = 0; i < count_all_cards; i++)//пробег по всем картам на столе
+        unsigned int discardedCardsCount = 0;//счётчик сброшенных карт
+        for (int i = 0; i < allCardsCount; i++)//пробег по всем картам на столе
         {
-            if (Cards[i].status == CardStatus::open)//если карта открыта
+            if (cards[i].status == CardStatus::opened)//если карта открыта
             {
-                sf::Texture* texture = getTextureByType(Cards[i].type, textures); // получить текстуру для этого типа карты
+                sf::Texture* texture = getTextureByType(cards[i].type, textures); // получить текстуру для этого типа карты
 
                 if (texture) {
-                    Cards[i].shape.setTexture(texture);//загрузить текстуру на карту
-                    Cards[i].shape.setFillColor(sf::Color::White);
+                    cards[i].shape.setTexture(texture);//загрузить текстуру на карту
+                    cards[i].shape.setFillColor(sf::Color::White);
                 }
             }
-            else if (Cards[i].status == CardStatus::close)//если карта закрыта
+            else if (cards[i].status == CardStatus::closed)//если карта закрыта
             {
 
-                Cards[i].shape.setTexture(nullptr);// убрать текстуру
-                Cards[i].shape.setFillColor(sf::Color::Green);//поставить зелёную рубашку
+                cards[i].shape.setTexture(nullptr);// убрать текстуру
+                cards[i].shape.setFillColor(sf::Color::Green);//поставить зелёную рубашку
 
             }
 
             else //если карта сброшена
             {
-                count_discarded_cards += 1;//считаем кол-во сброшенных карт
-                sf::Texture* texture = getTextureByType(Cards[i].type, textures); // получить текстуру для этого типа карты
+                discardedCardsCount += 1;//считаем кол-во сброшенных карт
+                sf::Texture* texture = getTextureByType(cards[i].type, textures); // получить текстуру для этого типа карты
 
                 if (texture) {
-                    Cards[i].shape.setTexture(texture);//надеть текстуру на карту
-                    Cards[i].shape.setFillColor(sf::Color::White);
+                    cards[i].shape.setTexture(texture);//надеть текстуру на карту
+                    cards[i].shape.setFillColor(sf::Color::White);
                 }
-                Cards[i].timer += dt;// зафиксировать когда сброшено
+                cards[i].timer += dt;// зафиксировать когда сброшено
 
-                if (Cards[i].timer >= 2.f)// если сброшено 2 секунды назад
+                if (cards[i].timer >= 2.f)// если сброшено 2 секунды назад
                 {
-                    Cards[i].shape.setTexture(nullptr);//убрать карту со стола = надеть чёрную текстуру 
-                    Cards[i].shape.setFillColor(sf::Color::Black);
+                    cards[i].shape.setTexture(nullptr);//убрать карту со стола = надеть чёрную текстуру 
+                    cards[i].shape.setFillColor(sf::Color::Black);
                 }
 
             }
 
-            if (count_discarded_cards == count_all_cards) // если все карты сброшены
+            if (discardedCardsCount == allCardsCount) // если все карты сброшены
             {
-                full_table(Cards);// заполнить стол новыми картами
-                for (int i = 0; i < count_all_cards; i++)
+                dealCards(cards);// заполнить стол новыми картами
+                for (int i = 0; i < allCardsCount; i++)
                 {
-                    Cards[i].timer = 0;// сброс таймера у всех карт
+                    cards[i].timer = 0;// сброс таймера у всех карт
                 }
 
             }
@@ -298,25 +298,25 @@ int main()
 
         int i;
         window.clear();// очистить окно
-        for (i = 0; i < (count_all_cards / 2); i++)
+        for (i = 0; i < (allCardsCount / 2); i++)
         {
-            Cards[i].shape.setPosition({ 100.f + i * 400, 100.f });//задаём позиции картам в 1 ряду
-            Cards[i].draw(window);
+            cards[i].shape.setPosition({ 100.f + i * 400, 100.f });//задаём позиции картам в 1 ряду
+            cards[i].draw(window);
         }
 
-        points_txt.setPosition({ 100.f + i * 400, 100.f });
-        points_txt.setString(std::to_string(points)); //задаём позицию очкам справа от 1 ряда
-        window.draw(points_txt);
+        txtPoints.setPosition({ 100.f + i * 400, 100.f });
+        txtPoints.setString(std::to_string(points)); //задаём позицию очкам справа от 1 ряда
+        window.draw(txtPoints);
 
-        for (i = count_all_cards / 2; i < count_all_cards; i++)
+        for (i = allCardsCount / 2; i < allCardsCount; i++)
         {
-            Cards[i].shape.setPosition({ 100.f + (i - count_all_cards / 2) * 400, 600.f });//задаём позиции картам в 2 ряду
-            Cards[i].draw(window);
+            cards[i].shape.setPosition({ 100.f + (i - allCardsCount / 2) * 400, 600.f });//задаём позиции картам в 2 ряду
+            cards[i].draw(window);
         }
 
-        timer_txt.setPosition({ 100.f + (i - count_all_cards / 2) * 400, 600.f });//задаём позицию таймеру справа от 2 ряда
-        timer_txt.setString(std::format("{:.2f}", 60 - gameTime));
-        window.draw(timer_txt);
+        txtTimer.setPosition({ 100.f + (i - allCardsCount / 2) * 400, 600.f });//задаём позицию таймеру справа от 2 ряда
+        txtTimer.setString(std::format("{:.2f}", 60 - gameTime));
+        window.draw(txtTimer);
 
 
         window.display();//выводим в окно всё что нарисовали
